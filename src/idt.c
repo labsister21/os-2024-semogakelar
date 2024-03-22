aa81903e0964cd1a3bcc47da2097272ab6115d4b
b33rk <brkmanics@gmail.com>
2024-03-22 15:27:05 +0700

    idt.c

--- src/idt.c
+++ src/idt.c
@@ -1,6 +1,13 @@
 #include "header/interrupt/idt.h"
 #include "header/cpu/gdt.h"
 
+struct InterruptDescriptorTable interrupt_descriptor_table;
+
+struct IDTR _idt_idtr = {
+    .size = sizeof(interrupt_descriptor_table) - 1,
+    .address = &interrupt_descriptor_table
+};
+
 void initialize_idt(void) {
     /* 
      * TODO: 
@@ -19,35 +26,6 @@ void initialize_idt(void) {
     __asm__ volatile("sti");
 }
 
-struct InterruptDescriptorTable interrupt_descriptor_table = {
-    .table = {
-        {
-            // TODO : Implement
-            .offset_low = 0,
-            .segment = 0,
-            ._reserved = 0,
-            ._r_bit_1 = 0,
-            ._r_bit_2 = 0,
-            ._r_bit_3 = 0,
-            .gate_32 = 0,
-            .dpl = 0,
-            .valid_bit = 0,
-            .offset_high = 0,
-        }
-    }
-};
-
-/**
- * _gdt_gdtr, predefined system GDTR. 
- * GDT pointed by this variable is already set to point interrupt_descriptor_table above.
- * From: https://wiki.osdev.org/Global_Descriptor_Table, GDTR.size is GDT size minus 1.
- */
-
-struct IDTR _idt_idtr = {
-    .size = sizeof(interrupt_descriptor_table) - 1,
-    .address = &interrupt_descriptor_table
-};
-
 void set_interrupt_gate(
     uint8_t  int_vector, 
     void     *handler_address, 
@@ -60,15 +38,13 @@ void set_interrupt_gate(
     // Target system 32-bit and flag this as valid interrupt gate
     uint32_t offset_handler = (uint32_t)handler_address;
     idt_int_gate->offset_low  = (uint32_t)(offset_handler &0xFFFF);
-    idt_int_gate->offset_high = (uint32_t)((offset_handler >> 16));
     idt_int_gate->segment = gdt_seg_selector;
-    idt_int_gate->dpl = privilege;
-
-    idt_int_gate->_reserved = 0b0;
+    idt_int_gate->_reserved = 0;
     idt_int_gate->_r_bit_1    = INTERRUPT_GATE_R_BIT_1;
     idt_int_gate->_r_bit_2    = INTERRUPT_GATE_R_BIT_2;
-    idt_int_gate->_r_bit_3    = INTERRUPT_GATE_R_BIT_3;
     idt_int_gate->gate_32     = 1;
+    idt_int_gate->_r_bit_3    = INTERRUPT_GATE_R_BIT_3;
+    idt_int_gate->dpl = privilege;
     idt_int_gate->valid_bit   = 1;
+    idt_int_gate->offset_high = (uint32_t)((offset_handler >> 16));
 }
-
--- src/kernel.c
+++ src/kernel.c
@@ -13,15 +13,15 @@ void kernel_setup(void) {
     pic_remap();
     initialize_idt();
     activate_keyboard_interrupt();
-    // framebuffer_clear();
+    framebuffer_clear();
     framebuffer_set_cursor(0, 0);
-    // __asm__("int $0x4");
+    __asm__("int $0x4");
         
-    int col = 0;
-    keyboard_state_activate();
-    while (true) {
-         char c;
-         get_keyboard_buffer(&c);
-         if (c) framebuffer_write(0, col++, c, 0xF, 0);
-    }
+    // int col = 0;
+    // keyboard_state_activate();
+    // while (true) {
+    //      char c;
+    //      get_keyboard_buffer(&c);
+    //      if (c) framebuffer_write(0, col++, c, 0xF, 0);
+    // }
 }
